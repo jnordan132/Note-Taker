95a4c18d6587a02860d0252ab42bc385cfccaefc
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-16 22:23:19 -0400

    implementing modular routing, still need to debug

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// Modules needed
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const dataBase = require('./db/db.json');
+const { createNote, deleteNote } = require('./helpers/functions')
 
 const PORT = 3001;
 const app = express();
@@ -12,13 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // HTML routes
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // API routes for notes page
 app.get('/api/notes', (req, res) => {
@@ -26,47 +25,17 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-        const newNote = createNote(req.body, dataBase);
-        res.json(newNote);
-    })
-    // Function to create new note and count ID for each note
-function createNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray.length;
-    notesArray[0]++;
-    notesArray.push(newNote);
-
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-};
+    const newNote = createNote(req.body, dataBase);
+    res.json(newNote);
+    createNote(req.body, dataBase);
+});
 
 app.delete('/api/notes/:id', (req, res) => {
-        deleteNote(req.params.id, dataBase);
-        res.json(true);
-    })
-    // Function to delete notes if user decided to
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
+    deleteNote(req.params.id, dataBase);
+    res.json(true);
+    deleteNote(id, notesArray);
+});
 
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-            break;
-        }
-    }
-};
 
 // App listenening at http://localhost:3001
 app.listen(PORT, () =>
